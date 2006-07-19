7b43d9734734a296c8ad7bad072e9afa43913e8b
PJ Eby <pje@telecommunity.com>
2006-07-19 23:07:36 +0000

    Split proxy types out of Contextual and into their own distribution. This way, the docs for Contextual don't need to explain them, and they're pretty useful on their own, anyway.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/ProxyTypes@2221 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Distutils setup file"""
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+
+# Metadata
+PACKAGE_NAME = "ProxyTypes"
+PACKAGE_VERSION = "0.9"
+PACKAGES = ['peak', 'peak.util']
+
+def get_description():
+    # Get our long description from the documentation
+    f = file('README.txt')
+    lines = []
+    for line in f:
+        if not line.strip():
+            break     # skip to first blank line
+    for line in f:
+        if line.startswith('.. contents::'):
+            break     # read to table of contents
+        lines.append(line)
+    f.close()
+    return ''.join(lines)
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    description="General purpose proxy and wrapper types",
+    long_description = open('README.txt').read(),
+
+    author="Phillip J. Eby",
+    author_email="peak@eby-sarna.com",
+    license="PSF or ZPL",
+    test_suite = 'test_proxies',
+    packages = PACKAGES,
+    namespace_packages = PACKAGES,
+)
+
